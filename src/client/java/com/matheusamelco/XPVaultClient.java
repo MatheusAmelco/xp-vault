c0d99eb1f91d40a63b5becdcae6f12ac723ab698
Matheus Amelco <matheusamelco@hotmail.com>
2024-07-16 21:09:13 -0300

    Template project

---
+++ src/client/java/com/matheusamelco/XPVaultClient.java
@@ -0,0 +1,10 @@
+package com.matheusamelco;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class XPVaultClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
